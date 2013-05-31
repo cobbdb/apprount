1fa50186735972b17dfba0fccf30dc84f13e667e
cobbdb <cobbdb@gmail.com>
2013-05-30 23:07:06 -0700

    Using templating for search line items.

--- res/index.js
+++ res/index.js
@@ -4,33 +4,33 @@ $(function () {
     sizeSearchBar();
 
     // Format query on form submit.
-    $("form").submit(function () {
+    $('form').submit(function () {
         // Commit any unentered title.
         addTitle();
-        
+
         // Build search query.
-        var query = "";
-        $("#list span").each(function () {
-            query += $(this).text() + "|";
+        var query = '';
+        $('#list span').each(function () {
+            query += $(this).text() + '|';
         });
         query = query.slice(0, -1);
-        
+
         // Populate hidden title field.
         $('input[type="hidden"]').val(query);
     });
 
     var pasteEvent = false;
-    $("textarea")
-        .on("paste", function () {
+    $('textarea')
+        .on('paste', function () {
             pasteEvent = true;
         })
-        .on("keyup", function () {
+        .on('keyup', function () {
             if (pasteEvent) {
                 pasteEvent = false;
                 addTitle();
             }
         })
-        .on("keypress", function (evt) {
+        .on('keypress', function (evt) {
             if (evt.which == 13) {
                 addTitle();
                 return false;
@@ -38,38 +38,37 @@ $(function () {
         });
 });
 
-
 // Set width of search bar.
 var sizeSearchBar = function () {
-    var wTotal = $("form").outerWidth();
-    var wDepth = $("#depth").outerWidth();
-    $("#titles").width(wTotal - wDepth - 14);
+    var wTotal = $('form').outerWidth();
+    var wDepth = $('#depth').outerWidth();
+    $('#titles').width(wTotal - wDepth - 14);
 };
-
 
 // Commit current entries to query list.
 function addTitle() {
-    var val = $("textarea").val().trim();
-    if (val != "") {
+    var searchInput = $('textarea').val().trim();
+    if (!_(searchInput).isEmpty()) {
         // Tokenize by line break.
-        var titles = $("textarea").val().split("\n");
-        for (var i in titles) {
-            var title = titles[i].trim();
-            if (title == "") {
-                continue;
+        var titles = $('textarea').val().split('\n');
+        // Create the line item template.
+        var tpl = _.template($('#titleTpl').text());
+
+        _(titles).each(function (title) {
+            title = title.trim();
+            if (!_(title).isEmpty()) {
+                $('#list')
+                    .append(tpl({
+                        title: title
+                    }))
+                    .find('img')
+                    .click(function (e) {
+                        $(e.target).parent().remove();
+                    });
             }
-            $("#list").append(' \
-<div> \
-    <img src="res/img/icon_trash.png" title="Remove title"/> \
-    <span>' + title + '</span> \
-</div> \
-            ');
-            $("#list img").click(function () {
-                $(this).parent().remove();
-            });
-        }
+        });
     }
-    $("textarea")
-        .val("")
-        .text("");
+    $('textarea')
+        .val('')
+        .text('');
 };
